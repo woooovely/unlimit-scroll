99ea8416000ac14dc49b404da0bb40e8808547cd
woosang <woosang3078@hanmail.net>
2023-08-03 17:43:38 +0900

    docs: 라이브러리명 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
-import { ReactQueryDevtools } from 'react-query-devtools';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { useRef } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
