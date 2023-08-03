fbce6ed5a9d3d03c6bfa6ee30d42835762cc4544
woosang <woosang3078@hanmail.net>
2023-08-03 16:48:29 +0900

    docs: queryClient 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Hydrate } from 'react-query/hydration';
+import { ReactQueryDevtools } from 'react-query-devtools';
+import { useRef } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClientRef = useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
+  return <>
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </>
 }
